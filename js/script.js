eeb44be46a3f63780ae17a845e874b35d2ae6a12
Lucas Trindade <lucasktrindade@gmail.com>
2017-01-06 14:52:33 -0200

    Adicionando exceção na trilha

--- js/script.js
+++ js/script.js
@@ -376,23 +376,36 @@ var paint = (function init(courses,goals,skills){
 
   switch(courses.length) {
     case 1:
-        $('.trilha1,.line-info').show();
-        $('.trilha2,.trilha3,.trilha4,.line-mensagem').hide();
+        $('.line-info').show();
+        $('.line-mensagem').hide();
         $('.areas').html(courses.length +' área');
         $('.spec-name').html('Certificado de Aperfeiçoamento em '+courses[0].nome);
         $('.hours').html((courses.length*180));
         $('.meses').html('6 meses de duração');
+        $('.trilha1').html("Trilha 1");
+        $('.trilha2,.trilha3,.trilha4').html("");
         let goal1 = findGoal(goals,courses[0].peso);
         let skill1 = findSkill(skills, courses[0].peso)
         $('.goal').html(goal1[0].descricao);
         $('.skill').html(skill1[0].descricao);
         break;
     case 2:
-        $('.trilha2,.line-info').show();
-        $('.trilha3,.trilha4,.line-mensagem').hide();
+        $('.line-info').show();
+        $('.line-mensagem').hide();
         $('.areas').html(courses.length +' áreas');
         $('.spec-name').html('Certificado de Especialização em Gestão de '+courses[0].nome+' e ' +courses[1].nome);
-        $('.meses').html('6 meses de duração');
+        if(courses[0].combDay == courses[1].combDay){
+          $('.meses').html('1 ano de duração');
+          $('.trilha1').html("Trilha 1");
+          $('.trilha3').html("Trilha 2");
+          $('.trilha2,.trilha4').html("");
+        }else{
+          $('.meses').html('6 meses de duração');
+          $('.trilha1').html("Trilha 1");
+          $('.trilha2').html("Trilha 2");
+          $('.trilha3,.trilha4').html("");
+        }
+
         $('.hours').html((courses.length*180)+90);
         let goal2 = findGoal(goals,peso(courses));
         let skill2 = findSkill(skills, peso(courses))
@@ -400,21 +413,29 @@ var paint = (function init(courses,goals,skills){
         $('.skill').html(skill2[0].descricao);
         break;
     case 3:
-        $('.trilha3,.line-info').show();
-        $('.trilha4,.line-mensagem').hide();
+        $('.line-info').show();
+        $('.line-mensagem').hide();
         $('.areas').html(courses.length +' áreas');
         $('.spec-name').html('Certificado de MBA em Gestão Essencial');
         $('.meses').html('1 ano de duração');
+        $('.trilha1').html("Trilha 1");
+        $('.trilha2').html("Trilha 2");
+        $('.trilha3').html("Trilha 3");
+        $('.trilha4').html("");
         $('.hours').html((courses.length*180)+90 );
         $('.goal').html("Fazer compreender os aspectos gerais de gestão de empresas e negócios, em suas visões essenciais dos aspectos de mercado e de marketing, da gestão de recursos financeiros, humanos e todo o processo de gestão estratégica e operacional dos recursos logísticos, de forma integrada e holística.");
         $('.skill').html("Ao final do curso o aluno estará apto a entender e aplicar os modelos de gestão das empresas considerando desde a análise e posicionamento de mercado, sua forma de atendimento ao consumidor, seu processo de produção e movimentação de produtos e serviços, sua gestão de recursos financeiros e apuração de resultados, bem como o processo de envolvimento das pessoas na gestão dos recursos e na motivação para a busca de resultados.");
         break;
     case 4:
-        $('.trilha4,.line-info').show();
+        $('.line-info').show();
         $('.line-mensagem').hide();
         $('.areas').html(courses.length +' áreas');
         $('.spec-name').html('Certificado de MBA em Gestão Avançado');
         $('.meses').html('1 ano de duração');
+        $('.trilha1').html("Trilha 1");
+        $('.trilha2').html("Trilha 2");
+        $('.trilha3').html("Trilha 3");
+        $('.trilha4').html("Trilha 4");
         $('.hours').html((courses.length*180)+90);
         $('.goal').html("Fazer compreender os aspectos gerais de gestão de empresas e negócios, em suas visões avançadas relacionadas aos aspectos de mercado e de markeeting, a gestão de recursos financeiros e humanos e todo o processo de gestão estratégica e operacional de recursos logísticos, de forma integrada e holística.");
         $('.skill').html("Ao final do curso o aluno estará apto a entender e aplicar os modelos de gestão das empresas considerando, de forma ampla e abrangente, desde a análise e posicionamento de mercado, sua forma de atendimento ao consumidor, seu processo de produção e movimentação de produtos e serviços, sua gestão de recursos financeiros e apuração de resultados, bem como o processo de envolvimento das pessoas na gestão dos recursos e na motivação para a busca de resultados.");
@@ -422,7 +443,7 @@ var paint = (function init(courses,goals,skills){
     default:
        $('.line-mensagem').show();
        $('.mensagem-none').html("<b>Simule a escolha de seu curso de Pós, selecionando de uma a quatro áreas de acordo com a necessidade da sua carreira</b>.<br>Clique na(s) área(s) de interesse e arraste para os campos pontilhados, para gerar a simulação.");
-       $('.trilhas,.line-info').hide();
+       $('.line-info').hide();
        $('.skill').html('');
        $('.goal').html('');
     }
